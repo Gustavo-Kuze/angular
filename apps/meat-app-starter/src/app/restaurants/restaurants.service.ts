436aa4606ada0ffe47c356bc6538cc6c8d64e478
Gustavo Kuze <gustavo.silva@rcadigital.com.br>
2019-03-21 13:37:48 -0300

    Serviço HTTP para o Detalhe do Restaurante

--- apps/meat-app-starter/src/app/restaurants/restaurants.service.ts
+++ apps/meat-app-starter/src/app/restaurants/restaurants.service.ts
@@ -4,7 +4,7 @@ import { Http } from '@angular/http';
 import { Restaurant } from './restaurant/restaurant.model'
 import { MEAT_API } from '../app.api'
 import { Observable } from 'rxjs/Observable';
-import {ErrorHandler} from '../app.error-handler'
+import { ErrorHandler } from '../app.error-handler'
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
 
@@ -15,6 +15,12 @@ export class RestaurantsService {
 
     restaurants = (): Observable<Restaurant[]> => {
         return this.http.get(`${MEAT_API}/restaurants`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError)
+    }
+
+    restaurantById(id: string): Observable<Restaurant> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}`)
         .map(response => response.json())
         .catch(ErrorHandler.handleError)
     }
