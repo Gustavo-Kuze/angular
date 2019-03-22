b2870b40a9cdfdf143fbca77bbc9d3a905ab418b
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2019-03-22 10:04:03 -0300

    Implementando o Carrinho de Compras

--- apps/meat-app-starter/src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ apps/meat-app-starter/src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from './shopping-cart.service';
 
 @Component({
   selector: 'mt-shopping-cart',
@@ -6,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingCartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private shoppingCartService: ShoppingCartService) { }
 
   ngOnInit() {
   }
+
+  items = () => <any[]>this.shoppingCartService.items
+
+  total = () => <number>this.shoppingCartService.total()
 
 }
