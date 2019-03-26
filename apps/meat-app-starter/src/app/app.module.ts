1ac03b75dcf42d3a29f95acafbb74ea7cb6297be
Gustavo Kuze <gustavo.silva@rcadigital.com.br>
2019-03-26 14:22:59 -0300

    Instalação do Módulo de Animações

--- apps/meat-app-starter/src/app/app.module.ts
+++ apps/meat-app-starter/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.compo
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
@@ -37,7 +38,8 @@ import { SharedModule } from './shared/shared.module';
     FormsModule,
     ReactiveFormsModule,
     SharedModule.forRoot(),
-    RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules})
+    RouterModule.forRoot(ROUTES, { preloadingStrategy: PreloadAllModules }),
+    BrowserAnimationsModule
   ],
   providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
   bootstrap: [AppComponent]
