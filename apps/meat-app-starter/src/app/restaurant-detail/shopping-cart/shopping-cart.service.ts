4cc57a238b798bb1b39b1c2c3d52b600eb66d364
Gustavo Kuze <gustavo.silva@rcadigital.com.br>
2019-03-26 15:13:05 -0300

    Usando Observable com Snackbar

--- apps/meat-app-starter/src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
+++ apps/meat-app-starter/src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
@@ -1,8 +1,13 @@
 import { CartItem } from "./cart-item.model";
 import { MenuItem } from "../menu-item/menu-item.model";
+import { Injectable } from "@angular/core";
+import { NotificationService } from "app/shared/messages/notification.service";
 
+@Injectable()
 export class ShoppingCartService {
     items: CartItem[] = []
+
+    constructor(private notificationService: NotificationService) { }
 
     clear() {
         this.items = []
@@ -15,10 +20,12 @@ export class ShoppingCartService {
         } else {
             this.items.push(new CartItem(item))
         }
+        this.notificationService.notify(`Você adicionou o item ${item.name}`)
     }
 
     removeItem(item: CartItem) {
         this.items.splice(this.items.indexOf(item), 1)
+        this.notificationService.notify(`Você removeu o item ${item.menuItem.name}`)
     }
 
     total = () => <number>(
@@ -28,9 +35,9 @@ export class ShoppingCartService {
     )
 
     increment = (item: CartItem) => item.quantity += 1
-    
+
     decrement = (item: CartItem) => {
         item.quantity -= 1
-        if(item.quantity === 0) this.removeItem(item)
+        if (item.quantity === 0) this.removeItem(item)
     }
 }
