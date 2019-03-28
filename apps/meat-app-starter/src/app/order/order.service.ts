f0a9aabec5d0ae17a7a5728dff40d64eb125bfa8
Gustavo Kuze <gustavo.silva@rcadigital.com.br>
2019-03-28 11:40:23 -0300

    Refactoring da Aplicação (Angular 4.3)

--- apps/meat-app-starter/src/app/order/order.service.ts
+++ apps/meat-app-starter/src/app/order/order.service.ts
@@ -3,14 +3,14 @@ import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shoppin
 import { CartItem } from "app/restaurant-detail/shopping-cart/cart-item.model";
 import { Observable } from "rxjs/Observable";
 import 'rxjs/add/operator/map'
-import { Http, RequestOptions, Headers } from '@angular/http'
+import { HttpClient } from '@angular/common/http'
 import { Order, OrderItem } from './order.model'
 import { MEAT_API } from '../app.api'
 
 @Injectable()
 export class OrderService {
 
-    constructor(private cartService: ShoppingCartService, private http: Http) { }
+    constructor(private cartService: ShoppingCartService, private http: HttpClient) { }
 
     cartItems = () => <CartItem[]>this.cartService.items
 
@@ -20,13 +20,7 @@ export class OrderService {
 
     itemsValue = () => this.cartService.total()
     checkOrder = (order: Order): Observable<string> => {
-        const headers = new Headers()
-        headers.append('Content-Type', 'application/json')
-        return this.http.post(
-            `${MEAT_API}/orders`,
-            JSON.stringify(order),
-            new RequestOptions({ headers }))
-            .map(resp => resp.json())
+        return this.http.post<Order>(`${MEAT_API}/orders`, order)
             .map(order => order.id)
     }
 
